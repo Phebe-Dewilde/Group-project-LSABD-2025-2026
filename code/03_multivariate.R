e0597b9f68e45eac0948e4c19a0e4926ca616d4f
xhaentje <xander.haentjens@ugent.be>
2025-12-12 22:17:42 +0100

    Update 03_multivariate.R

--- code/03_multivariate.R
+++ code/03_multivariate.R
@@ -33,28 +33,105 @@ top_gene
 df_finaal <- df_filtered_colon%>%
   mutate(label_site_label = recode(label_site_label, "Primary" = 0, "Metastatic" = 1, .default = NA_real_))
 
-## 8. Logistic Regression Models
+## 8. Linearity assumption check
+library(car)
 
-## 8.1 Individual (No interactions)
+df_tested <- df_finaal %>% 
+  select(label_site_label, label_age, label_gender, HOXB6_1)
+
+df_tested$label_age <- df_tested$label_age + abs(min(df_tested$label_age)) + 0.01
+df_tested$HOXB6_1 <- df_tested$HOXB6_1 + abs(min(df_tested$HOXB6_1)) + 0.01
+
+model_tested <- glm(
+  label_site_label ~ label_age + HOXB6_1 +
+    label_age:log(label_age) + HOXB6_1:log(HOXB6_1),
+  family = binomial,
+  data = df_tested
+)
+summary(model_tested)
+
+## 9. Logistic Regression Models
+
+## 9.1 Individual (No interactions)
 model_1 <- glm(
   label_site_label ~ label_age + label_gender + HOXB6_1,
   data = df_finaal,
   family = binomial
 )
 summary(model_1)
+vif(model_1)
 
-## 8.2 Without_gender (Not significant)
+## 9.2 Without_gender (Not significant)
 model_2 <- glm(
   label_site_label ~ label_age  + HOXB6_1,
   data = df_finaal,
   family = binomial
 )
 summary(model_2)
+vif(model_2)
 
-## 8.3 All interactions
+## 9.3 All interactions
 model_interactie <- glm(
   label_site_label ~ label_age * HOXB6_1 + label_age + HOXB6_1 + label_gender + label_gender*label_age + label_gender*HOXB6_1,
   data = df_finaal,
   family = binomial
 )
 summary(model_interactie)
+
+## 10. Visualisations
+
+## 10.1 Violin Plot HOXB6_1 methylation
+ggplot(df_finaal, aes(x = factor(label_site_label), y = HOXB6_1, fill = factor(label_site_label))) +
+  geom_violin(alpha = 0.6) +
+  geom_boxplot(width = 0.1, color = "black") +
+  scale_x_discrete(labels = c("0" = "Primary", "1" = "Metastatic")) +
+  scale_fill_manual(values = c("0" = "lightcoral", "1" = "turquoise"),
+                    labels = c("Primary", "Metastatic"),
+                    name = 'Metastasis status') +
+  
+  labs(
+    title = "HOXB6_1 methylation per metastasis status",
+    x = "Metastasis status",
+    y = "HOXB6_1 methylation"
+  ) +
+  theme_minimal()
+
+## 10.2 Violin Plot Age
+ggplot(df_finaal, aes(x = factor(label_site_label), y = label_age, fill = factor(label_site_label))) +
+geom_violin(alpha = 0.6) +
+geom_boxplot(width = 0.1, color = "black") +
+scale_x_discrete(labels = c("0" = "Primary", "1" = "Metastatic")) +
+scale_fill_manual(values = c("0" = "lightcoral", "1" = "turquoise"),
+labels = c("Primary", "Metastatic"),
+name = 'Metastasis status') +
+labs(
+title = "Age per metastasis status",
+x = "Metastasis status",
+y = "Age"
+) +
+theme_minimal()
+
+## 10.3 
+library(ggplot2)
+
+plotdata <- expand.grid(
+  HOXB6_1 = seq(min(df_finaal$HOXB6_1), max(df_finaal$HOXB6_1), length.out = 100),
+  label_age = seq(min(df_finaal$label_age), max(df_finaal$label_age), length.out = 100)
+)
+
+plotdata$pred_prob <- predict(
+  glm(label_site_label ~ label_age + HOXB6_1, 
+      family = binomial, data = df_finaal),
+  newdata = newdata, type = "response"
+)
+
+ggplot(plotdata, aes(x = HOXB6_1, y = label_age, z = pred_prob)) +
+  geom_tile(aes(fill = pred_prob)) +
+  geom_contour(color = "white", alpha = 0.7) +
+  scale_fill_viridis_c(name = "Predicted probability of metastasis") +
+  labs(
+    x = "HOXB6 methylation (beta value)",
+    y = "Age",
+    title = "Predicted probability of metastasis based on HOXB6 and age"
+  ) +
+  theme_minimal()
